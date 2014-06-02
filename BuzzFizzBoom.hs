8bd238e1413b6314ed01d2d819b7423f786e5c34
Stanislav Ursache <d34dkn16h7@gmail.com>
2014-06-02 23:36:16 +0300

    bfb update

--- BuzzFizzBoom.hs
+++ BuzzFizzBoom.hs
@@ -1,17 +1,13 @@
 module Main where
 
-main = print' [1 .. 100]
+main = p [1 .. 100]
 
-print' [] = return ()
-print' (x:xs) | mod x 5 == 0 && mod x 3 == 0 = do
-												print "FizzBuzz"
-												print' xs
-			  | mod x 5 == 0 = do
-												print "Buzz"
-												print' xs
-			  | mod x 3 == 0 = do
-												print "Fizz"
-												print' xs
-			  | otherwise = do
-												print x
-												print' xs
+p [] = return ()
+p (x:xs) = do 
+	print' x
+	p xs
+
+print' x | mod x 5 == 0 && mod x 3 == 0 = do print "FizzBuzz"
+         | mod x 5 == 0 = do print "Buzz"
+         | mod x 3 == 0 = do print "Fizz"
+		 | otherwise = do print x
