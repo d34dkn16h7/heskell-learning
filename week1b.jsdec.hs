712d0b4fcd5189bad7c886e865cd6406f5074547
Stanislav Ursache <d34dkn16h7@gmail.com>
2014-06-23 17:43:28 +0300

    cleaner jsdec

--- week1b.jsdec.hs
+++ week1b.jsdec.hs
@@ -2,16 +2,14 @@ main = print $ charRemap "dllkhGlgME" ['a' .. 'm'] ['z','y' .. 'n']
 
 charRemap :: [Char] -> [Char] -> [Char] -> [Char]
 charRemap [] _ _ = []
-charRemap (w:word) mapFrom mapTo = if hasChar w mapFrom
-							   	   then (mapTo !! (charIndex w mapFrom 0)) : charRemap word mapFrom mapTo
-							       else w : charRemap word mapFrom mapTo
+charRemap (w:word) mapFrom mapTo =
+	if snd state
+		then mapTo !! fst state : charRemap word mapFrom mapTo
+		else w : charRemap word mapFrom mapTo
+	where
+		state = hasChar w mapFrom 0
 
-hasChar :: Char -> [Char] -> Bool
-hasChar _ [] = False
-hasChar c (x:xs) | c == x = True
-				 | otherwise = hasChar c xs
-
-charIndex :: Char -> [Char] -> Int -> Int
-charIndex _ [] _ = -1
-charIndex c (x:xs) ind | c == x = ind
-				   	   | otherwise = charIndex c xs (ind+1)
+hasChar :: Char -> [Char] -> Int -> (Int,Bool)
+hasChar _ [] _ = (0,False)
+hasChar c (x:xs) i | c == x = (i,True)
+				   | otherwise = hasChar c xs (i+1)
